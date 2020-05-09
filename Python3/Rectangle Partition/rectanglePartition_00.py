5927f01aace12b443e9b0beecaa0730eff26bcff
reaperlord <ffpffake@hotmail.com>
2020-05-09 09:45:07 +0200

    my first pr

---
+++ Python3/Rectangle Partition/rectanglePartition_00.py
@@ -0,0 +1,17 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+w, h, count_x, count_y = [int(i) for i in input().split()]
+for i in input().split():
+    x = int(i)
+for i in input().split():
+    y = int(i)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr)
+
+print("0")
+
